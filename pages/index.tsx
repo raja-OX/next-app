46416b49501fd432e54a8c88a3b3c722c68a9145
RajaARK <raja@oxsoftwares.com>
2022-08-27 14:37:37 +0530

    Test the deployment.

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ const Home: NextPage = () => {
   return (
     <div className={"h-screen w-full grid place-content-center"}>
       <p>Hello Next world</p>
+      <p>This is demo application.</p>
     </div>
   );
 };
