6582c3f795ab0bc0bfc281080bc769feef6c86b6
RajaARK <raja@oxsoftwares.com>
2022-08-27 14:44:01 +0530

    Third commit.

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,6 @@ const Home: NextPage = () => {
   return (
     <div className={"h-screen w-full grid place-content-center"}>
       <p>Hello Next world</p>
-      <p>This is demo application.</p>
     </div>
   );
 };
